a366588e16ee43e2cf558df5f8b4ff213ad227a0
SayemTutorial <63445800+SayemTutorial@users.noreply.github.com>
2020-09-26 11:38:10 +0600

    Create twLSN.js

---
+++ twLSN.js
@@ -0,0 +1,9 @@
+var twLSN = [
+'tokowhatsapp.blogspot.com',
+'toko-whatsapp.blogspot.com',
+'jasa-design.web.id',
+'www.jasa-design.web.id',
+'about-blanter-sayem-code.blogspot.com'
+];
+
+var redirectUrl = 'www.sayemtutorial.com';
